a7a6772f9c633a276ad5ec4ef6e6f85b26bfb284
Martin Stottmeister <martyumbrella@hotmail.com>
2025-06-13 18:37:44 +0200

    adding tkinter again, but without gui

--- test/ui/ui_menus/test_zoom.py
+++ test/ui/ui_menus/test_zoom.py
@@ -1,10 +1,15 @@
 # pylint: disable=redefined-outer-name
+import tkinter as tk
 from unittest.mock import MagicMock, patch
 
 import pytest
 from PIL import Image
 
 from ui.ui_menus.zoom import ZoomManager
+
+# Create a hidden Tkinter root window for tests
+root = tk.Tk()
+root.withdraw()  # Prevent it from showing a GUI
 
 
 @pytest.fixture
